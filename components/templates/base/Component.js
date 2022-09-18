e2f08f2dc52b7b396e9f649285c3222c5bcdc17f
charlesmarvin <marvin.charles@gmail.com>
2022-09-18 14:14:14 -0400

    feat: init template project

---
+++ components/templates/base/Component.js
@@ -0,0 +1,7 @@
+import styles from './Component.module.css'
+
+const Component = () => {
+  return <div className={styles.container}>This is a Component</div>
+}
+
+export default Component
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    // domains: ['<ADD_IMAGE_DOMAIN_HERE>'],
+  },
+}
+
+module.exports = nextConfig
